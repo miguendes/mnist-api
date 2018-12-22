d781c9dc4318d85b94fc27efb24c09a4ffa27487
Miguel Mendes <miguel.mdebrito@gmail.com>
2018-12-22 10:40:02 -0200

    Refactor test cases with pytest

--- tests/test_app.py
+++ tests/test_app.py
@@ -33,8 +33,8 @@ class TestMNISTApp(unittest.TestCase):
     def test_home(self):
         """Tests if accessing the index the welcome message is the same as defined in the endpoint."""
         response = self.client.get('/')
-        self.assertEqual(200, response.status_code)
-        self.assertIn('Web API | MNIST Challenge', response.data.decode('utf-8'))
+        assert 200 == response.status_code
+        assert 'Web API | MNIST Challenge' in response.data.decode('utf-8')
 
     @mock.patch('app.ml_models.fetch')
     def test_predict_with_image_from_dataset(self, fetch_model_mock):
@@ -63,8 +63,8 @@ class TestMNISTApp(unittest.TestCase):
         fetch_model_mock.assert_called_once_with(model_name)
         model_mock.predict.assert_called_once_with(image_b64)
 
-        self.assertEqual(200, response.status_code)
-        self.assertEqual({"prediction": 9}, json.loads(response.get_data(as_text=True)))
+        assert 200 == response.status_code
+        assert {"prediction": 9} == json.loads(response.get_data(as_text=True))
 
     def test_predict_with_real_model(self):
         """Tests if given a valid JSON, the prediction is returned. """
@@ -74,13 +74,13 @@ class TestMNISTApp(unittest.TestCase):
 
         response = self.client.post('/predict/', json={'model': model_name, 'image': image_b64})
 
-        self.assertEqual(200, response.status_code)
-        self.assertEqual({"prediction": 4}, json.loads(response.get_data(as_text=True)))
+        assert 200 == response.status_code
+        assert {"prediction": 4} == json.loads(response.get_data(as_text=True))
 
     def test_list_models(self):
         response = self.client.get('/models/')
-        self.assertEqual(200, response.status_code)
-        self.assertEqual({"models": ["cnn", "mlp", "svm"]}, json.loads(response.get_data(as_text=True)))
+        assert 200 == response.status_code
+        assert {"models": ["cnn", "mlp", "svm"]} == json.loads(response.get_data(as_text=True))
 
     def tearDown(self):
         """Tear down method to get rid of flask context created."""
